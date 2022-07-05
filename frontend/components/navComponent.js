caebea5d461a9cb07dbd5b04f5c045f328e6db04
Christian Smith Mantas <casm202@gmail.com>
2022-07-05 20:54:23 +0200

    Create: navbar component and styling

---
+++ frontend/components/navComponent.js
@@ -0,0 +1,42 @@
+// Declaration of navigation bar component, render with care ♥
+class NavigationBar {
+
+    constructor(paramsObj) {
+
+    }
+
+	getCategories(data) {
+		let catList = '';
+		for (let filter in data) {
+			catList += `
+				<a href="/category/${this.types[filter].type}" class="dropdown_link" router-link>${this.types[filter].type}</a>
+			`;
+		}
+		return catList;
+	}
+
+    async getHtml () {
+
+		const fetchData = await fetch('/api/joke-types', { method: 'GET' });
+        const resData = await fetchData.json();
+        this.types = resData;
+
+        return `
+			<nav>
+				<p class="title"><a href="/" router-link>Jokes!</a></p>
+
+				<ul class="navLinks">
+					<li><a href="/" router-link>ramdom</a></li>
+					<li>
+						<div class="dropdown">
+							<button class="dropdown_button">categories +</button>
+							<div class="dropdown_content">
+								${this.getCategories(this.types)}
+							</div>
+						</div>
+					</li>
+				</ul>
+			</nav>
+        `;
+    }
+}
